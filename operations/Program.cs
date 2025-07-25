d319a8c46d19f2236c9d692f8a4045da24778756
dpranaydeshmukh <dpranay182@gmail.com>
2025-07-25 08:48:49 +0530

    Add project files.

---
+++ operations/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operations
+{
+    internal class Program
+    {
+        public void Addition()
+        {
+            int a=10, b=20, c;
+            c = a + b;
+            Console.WriteLine("Addition="+c);
+        }
+        static void Main(string[] args)
+        {
+            Program P = new Program();
+            P.Addition();
+            Console.ReadLine();
+        }
+    }
+}
